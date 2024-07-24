a096caeff59c47c5742001d128e39c96b5749e91
Mihail-Kamenev <132842014+Mihail-Kamenev@users.noreply.github.com>
2024-07-24 12:14:18 +0300

    Add files via upload

---
+++ lesson_19/js/script.js
@@ -0,0 +1,6 @@
+document.addEventListener('click', function (e) {
+	const targetElement = e.target;
+	if (targetElement.closest('.icon-menu')) {
+		document.documentElement.classList.toggle('menu-open');
+	}
+})
